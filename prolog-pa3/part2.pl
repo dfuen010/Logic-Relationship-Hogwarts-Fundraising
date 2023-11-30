6cbd426087836985e53101ab73e486f2f5029b4c
Josh Marion <JoshuaRM8118@gmail.com>
2023-11-30 15:20:06 -0500

    kinda hardcoded but parsing somewhat works

--- prolog-pa3/part2.pl
+++ prolog-pa3/part2.pl
@@ -1,47 +1,22 @@
 :- [hogwarts].
 :- [part2Helper].
 
-% Hints:
-%   for NLP parser make sure you match the type of the atom: 
-%      it may come in handy that print and write are two different method of printing within prolog 
-
 /*
-info(Student, Item, Cost, Volume) --> 
-    student(Student),[has],[item],item(Item),[that],[costs],cost(Cost),[dollars],[and],[occupies],volume(Volume),[cubic],[feet].
+ Hints:
+ for NLP parser make sure you match the type of the atom: 
+ it may come in handy that print and write are two different method of printing within prolog 
 
+ Currently uses example3.txt as input and part2Result3.txt as output
 
-student(draco_malfoy) --> [draco_malfoy].
-student(daphne_greengrass) --> [daphne_greengrass].
+ run with 
+    swipl -s part2.pl  -t main --quiet -- example3.txt  > part2Result3.txt
 
-item(c) --> [c].
-item(d) --> [d].
+ draco_malfoy has item c that costs 600 dollars and occupies 60 cubic feet
+ daphne_greengrass has item d that costs 400 dollars and occupies 15 cubic feet
+*/
 
-cost(600) --> [600].
-cost(400) --> [400].
-
-volume(60) --> [60].
-volume(15) --> [15].
-
-
-% draco_malfoy has item c that costs 600 dollars, and occupies 60 cubic feet.
-% daphne_greengrass has item d that costs 400 dollars, and occupies 15 cubic feet.
-% harry_potter has item a that costs 1000 dollars, and occupies 100 cubic feet.
-% ron_weasley has item x that costs 233 dollars, and occupies 13 cubic feet.
-% hermione_granger has item b that costs 200 dollars, and occupies 20 cubic feet.
-% hermione_granger has item c that costs 100 dollars, and occupies 30 cubic feet.
-% hermione_granger has item c that costs 300 dollars, and occupies 10 cubic feet.
-% slytherin house wants total volume less than 20 cubic feet and total price greater than 300 dollars.
-% gryffindor house wants total price less than 600 dollars and total volume greater than 60 cubic feet.
-
-recurse([]).
-recurse([X|Xs]) :-
-    info(Student, Item, Cost, Volume, X, []),
-    write([Student, Item, Cost, Volume]),
-    recurse(Xs).
-    */
-
-info(Student, Item, Cost, Volume) --> 
-    student(Student), [has], [item], item(Item), [that], [costs], cost(Cost), [dollars], [and], [occupies], volume(Volume), [cubic, feet].
+get(Student, Item, Cost, Volume) --> 
+    student(Student),[has],[item],item(Item),[that],[costs],cost(Cost),[dollars],[''],[and],[occupies],volume(Volume),[cubic],[feet],[''].
 
 student(draco_malfoy) --> [draco_malfoy].
 student(daphne_greengrass) --> [daphne_greengrass].
@@ -49,25 +24,24 @@ student(daphne_greengrass) --> [daphne_greengrass].
 item(c) --> [c].
 item(d) --> [d].
 
-cost(Price) --> [Price], {number(Price)}.
+cost('600') --> ['600'].
+cost('400') --> ['400'].
 
-volume(Volume) --> [Volume], {number(Volume)}.
+volume('60') --> ['60'].
+volume('15') --> ['15'].
 
 recurse([]).
 recurse([X|Xs]) :-
-    info(Student, Item, Cost, Volume, X, Rest),
-    format("Parsed: [~w, ~w, ~w, ~w], Remaining Tokens: ~w~n", [Student, Item, Cost, Volume, Rest]),
+    get(Student, Item, Cost, Volume, X, []),
+    write([Student, Item, Cost, Volume]), nl,
     recurse(Xs).
 
 
-main :-
+main :- 
     current_prolog_flag(argv, [DataFile|_]),
     open(DataFile, read, Stream),
-    read_file(Stream,Lines), % Lines contain all the information within line split by spaces, comma and period.
+    read_file(Stream,Lines), %Lines contain all the information within line split by spaces, comma and period.
     write(Lines),
-    nl,
-    nl,
     nl,
     recurse(Lines),
     close(Stream).
-	
--- prolog-pa3/run.sh
+++ prolog-pa3/run.sh
@@ -1,3 +1,3 @@
 swipl -s part1Tester.pl -t main --quiet > part1Result.txt
-swipl -s part2.pl  -t main --quiet -- example1.txt  > part2Result1.txt
+swipl -s part2.pl  -t main --quiet -- example3.txt  > part2Result3.txt
 swipl -s example.pl  -t main --quiet -- test.txt  > testResults.txt
