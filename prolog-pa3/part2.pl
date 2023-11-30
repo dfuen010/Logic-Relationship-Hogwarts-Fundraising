6302cd00cedb63b1e5395d353e63c5f5e3e3d3ff
Josh Marion <JoshuaRM8118@gmail.com>
2023-11-30 15:57:31 -0500

    its not looking good for us

--- prolog-pa3/part2.pl
+++ prolog-pa3/part2.pl
@@ -11,30 +11,116 @@
  run with 
     swipl -s part2.pl  -t main --quiet -- example3.txt  > part2Result3.txt
 
- draco_malfoy has item c that costs 600 dollars and occupies 60 cubic feet
- daphne_greengrass has item d that costs 400 dollars and occupies 15 cubic feet
+
+susan_bones has item x that costs 400 dollars, and occupies 10 cubic feet.
+eli_olivander has item y that costs 260 dollars, and occupies 15 cubic feet.
+vince_glover has item z that costs 490 dollars, and occupies 35 cubic feet.
+terry_boot has item u that costs 350 dollars, and occupies 50 cubic feet.
+
+duncan_inglebee has item v that costs 125 dollars, and occupies 26 cubic feet.
+cho_chang has item w that costs 235 dollars, and occupies 34 cubic feet.
+draco_malfoy has item c that costs 600 dollars, and occupies 60 cubic feet.
+terence_higgs has item c that costs 1200 dollars, and occupies 60 cubic feet.
+
+daphne_greengrass has item d that costs 400 dollars, and occupies 15 cubic feet.
+harry_potter has item a that costs 1000 dollars, and occupies 100 cubic feet.
+ron_weasley has item d that costs 233 dollars, and occupies 13 cubic feet.
+
+hermione_granger has item b that costs 200 dollars, and occupies 20 cubic feet.
+hermione_granger has item c that costs 100 dollars, and occupies 30 cubic feet.
+hermione_granger has item c that costs 300 dollars, and occupies 10 cubic feet.
+
+gryffindor house wants total price greater than 500 dollars and total volume greater than 60 cubic feet.
+slytherin house wants total volume less than 50 cubic feet and total price greater than 600 dollars.
+ravenclaw house wants total volume greater than 80 cubic feet and total price greater than 700 dollars.
+hufflepuff house wants total volume less than 20 cubic feet and total price greater than 250 dollars.
+
 */
 
-get(Student, Item, Cost, Volume) --> 
+getStudent(Student, Item, Cost, Volume) --> 
     student(Student),[has],[item],item(Item),[that],[costs],cost(Cost),[dollars],[''],[and],[occupies],volume(Volume),[cubic],[feet],[''].
 
-student(draco_malfoy) --> [draco_malfoy].
-student(daphne_greengrass) --> [daphne_greengrass].
+getHouse(House, CostLimit, VolumeLimit) -->
+    house(House),[house],[wants],[total],
 
+student(susan_bones) --> [susan_bones].
+student(eli_olivander) --> [eli_olivander].
+student(vince_glover) --> [vince_glover].
+student(terry_boot) --> [terry_boot].
+
+student(duncan_inglebee) --> [duncan_inglebee].
+student(cho_chang) --> [cho_chang].
+student(draco_malfoy) --> [draco_malfoy].
+student(terence_higgs) --> [terence_higgs].
+
+student(daphne_greengrass) --> [daphne_greengrass].
+student(harry_potter) --> [harry_potter].
+student(ron_weasley) --> [ron_weasley].
+
+student(hermione_granger) --> [hermione_granger].
+
+item(a) --> [a].
+item(b) --> [b].
 item(c) --> [c].
 item(d) --> [d].
 
-cost('600') --> ['600'].
+item(v) --> [v].
+item(w) --> [w].
+
+item(x) --> [x].
+item(y) --> [y].
+item(z) --> [z].
+item(u) --> [u].
+
 cost('400') --> ['400'].
+cost('260') --> ['260'].
+cost('490') --> ['490'].
+cost('350') --> ['350'].
 
-volume('60') --> ['60'].
+cost('125') --> ['125'].
+cost('235') --> ['235'].
+cost('600') --> ['600'].
+cost('1200') --> ['1200'].
+
+cost('400') --> ['400'].
+cost('1000') --> ['1000'].
+cost('233') --> ['233'].
+
+cost('200') --> ['200'].
+cost('100') --> ['100'].
+cost('300') --> ['300'].
+
+
+volume('10') --> ['10'].
 volume('15') --> ['15'].
+volume('35') --> ['35'].
+volume('50') --> ['50'].
 
-recurse([]).
-recurse([X|Xs]) :-
-    get(Student, Item, Cost, Volume, X, []),
+volume('26') --> ['26'].
+volume('34') --> ['34'].
+volume('60') --> ['60'].
+
+
+volume('15') --> ['15'].
+volume('100') --> ['100'].
+volume('13') --> ['13'].
+
+
+volume('20') --> ['20'].
+volume('30') --> ['30'].
+volume('10') --> ['10'].
+
+house(gryffindor) --> [gryffindor].
+house(slytherin) --> [slytherin].
+house(ravenclaw) --> [ravenclaw].
+house(hufflepuff) --> [hufflepuff].
+
+
+recurseStudent([]).
+recurseStudent([X|Xs]) :-
+    getStudent(Student, Item, Cost, Volume, X, []),
     write([Student, Item, Cost, Volume]), nl,
-    recurse(Xs).
+    recurseStudent(Xs).
 
 
 main :- 
@@ -43,5 +129,5 @@ main :-
     read_file(Stream,Lines), %Lines contain all the information within line split by spaces, comma and period.
     write(Lines),
     nl,
-    recurse(Lines),
+    recurseStudent(Lines),
     close(Stream).
