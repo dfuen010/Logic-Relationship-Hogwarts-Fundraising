de0d75920671f1bc74f879b38332678c64fd93f1
dfuen010 <dfuen010@gmail.com>
2023-12-01 18:15:35 -0500

    got list of items for each house, still need to go through them and find valid combinations for constraint

--- prolog-pa3/part2.pl
+++ prolog-pa3/part2.pl
@@ -3,12 +3,12 @@
 
 /*
  Hints:
- for NLP parser make sure you match the type of the atom: 
- it may come in handy that print and write are two different method of printing within prolog 
+ for NLP parser make sure you match the type of the atom:
+ it may come in handy that print and write are two different method of printing within prolog
 
  Currently uses example3.txt as input and part2Result3.txt as output
 
- run with 
+ run with
     swipl -s part2.pl  -t main --quiet -- example3.txt  > part2Result3.txt
 
 
@@ -37,7 +37,7 @@ hufflepuff house wants total volume less than 20 cubic feet and total price grea
 
 */
 
-parse(Student, Item, Cost, Volume) --> 
+parse(Student, Item, Cost, Volume) -->
     student(Student),[has],[item],item(Item),[that],[costs],cost(Cost),[dollars],[''],[and],[occupies],volume(Volume),[cubic,feet],[''].
 
 parse(House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2) -->
@@ -69,19 +69,112 @@ cost(C) --> [C].
 volume(V) --> [V].
 house(H) --> [H].
 
+:- dynamic house_Prices/2.
+:- dynamic house_Areas/2.
+:- dynamic house_Items/2.
+:- dynamic house_totalPrice/2.
+:- dynamic house_totalArea/2.
+
+printCostsInHouse(House) :-
+    write('Costs in '), write(House), write(': '), nl,
+    house_Prices(House, Cost),
+    write('- '), writeln(Cost),
+    house_totalPrice(House, TCost),
+    write('Total Cost is '), writeln(TCost),
+    fail.
+printCostsInHouse(_).
+
+convertStringToInt(String, Int) :-
+    atom_number(String, Int).
+
+%function for adding the cost of someone's item to the house currently
+addToHouseProfit(House, NCost) :-
+    (
+        retract(house_totalPrice(House, Total)),
+        %write(Total),
+        NewTotal is Total + NCost,
+        %write(NewTotal),
+        assert(house_totalPrice(House, NewTotal))
+        ;
+        assert(house_totalPrice(House, NCost))
+    ).
+
+addToHouseVolume(House, NVolume) :-
+    (
+        retract(house_totalVolume(House, Total)),
+        NewTotal is Total + NVolume,
+        assert(house_totalPrice(House, NewTotal))
+        ;
+        assert(house_totalPrice(House, NVolume))
+    ).
+
+generateItemList(House, Attribute1, Comparison1, Value1, Attribute2, Comparison2, Value2) :-
+    % Convert string values to integers
+    convertStringToInt(Value1, IntValue1),
+    convertStringToInt(Value2, IntValue2),
+
+    % Retrieve the relevant house data
+    house_Items(House, ItemList),
+    write(ItemList),
+    generateItemListHelper(ItemList, Attribute1, Comparison1, IntValue1, Attribute2, Comparison2, IntValue2, []).
+
+generateItemListHelper([], _, _, _, _, _, _, Result) :-
+    % Base case: reached the end of the list
+    writeln(Result).
+
+generateItemListHelper([Item|Rest], Attribute1, Comparison1, Value1, Attribute2, Comparison2, Value2, Acc) :-
+    % Check if the current item satisfies both constraints
+    checkItemConstraints(Item, Attribute1, Comparison1, Value1),
+    checkItemConstraints(Item, Attribute2, Comparison2, Value2),
+    % If satisfied, add to the accumulator
+    (
+    generateItemListHelper(Rest, Attribute1, Comparison1, Value1, Attribute2, Comparison2, Value2, [Item|Acc])
+         ;
+    generateItemListHelper(Rest, Attribute1, Comparison1, Value1, Attribute2, Comparison2, Value2, [Acc])
+    ).
+
+checkItemConstraints(Item, Attribute, Comparison, Value) :-
+    % Check the constraints for a single item
+    % (Similar to the checkConstraint/4 predicate but applied to each item)
+    (
+        Comparison = 'greater',
+        item(Attribute, Item, ActualValue),
+        ActualValue > Value
+        ;
+        Comparison = 'less',
+        item(Attribute, Item, ActualValue),
+        ActualValue < Value
+    ).
+
+
 /* This receives the whole paragraph, and goes through and prints line by line */
 recurseStudent([]).
 recurseStudent([X|Xs]) :-
 % Needs to parse correct one
     (
-        parse(Student, Item, Cost, Volume, X, []) -> write([Student, Item, Cost, Volume]), nl
+        parse(Student, Item, Cost, Volume, X, []) -> write([Student, Item, Cost, Volume]), nl,
+        houseOf(House, Student),
+        convertStringToInt(Cost, NCost),
+        assert(house_Prices(House, NCost)),
+
+        addToHouseProfit(House, NCost),
+
+        convertStringToInt(Volume, NVolume),
+        assert(house_Areas(House, NVolume)),
+
+        addToHouseVolume(House, NVolume),
+
+        assert(house_Items(House, Item)),
+        printCostsInHouse(House)
         ;
         parse(House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2, X, []),
+
+        generateItemList(House, Attribute1, Comparison1, Value1, Attribute2, Comparison2, Value2),
         write([House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2]), nl
     ),
     recurseStudent(Xs).
 
-main :- 
+main :-
     current_prolog_flag(argv, [DataFile|_]),
     open(DataFile, read, Stream),
     read_file(Stream,Lines), %Lines contain all the information within line split by spaces, comma and period.
