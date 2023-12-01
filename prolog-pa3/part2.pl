5525a6c485eb5a02b7507084ff5d56358d7f8b25
Josh Marion <JoshuaRM8118@gmail.com>
2023-11-30 19:32:13 -0500

    I fixed the hardcoding lol

--- prolog-pa3/part2.pl
+++ prolog-pa3/part2.pl
@@ -44,115 +44,30 @@ parse(House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Compariso
     house(House),[house],[wants],[total],attribute_value(Attribute1, Comparison1, Than, Value1, Unit1),[and],[total],
     attribute_value(Attribute2, Comparison2, Than, Value2, Unit2),[''].
 
-% gryffindor house wants total price greater than 500 dollars and total volume greater than 60 cubic feet.
-% slytherin house wants total volume less than 50 cubic feet and total price greater than 600 dollars.
-% Facts
 attribute_value(Attribute, Comparison, Than, Value, Unit) -->
     attribute(Attribute),
     comparison(Comparison, Than),
     value(Value),
     unit(Unit).
 
-% Rules
 attribute(price) --> [price].
 attribute(volume) --> [volume].
-
 
 comparison(greater,than) --> [greater, than].
 comparison(less,than) --> [less, than].
 
-
-
 % value(x) --> atom_number(x,y).
-
-value('50') --> ['50'].
-value('80') --> ['80'].
-value('20') --> ['20'].
-value('60') --> ['60'].
-value('600') --> ['600'].
-value('700') --> ['700'].
-value('250') --> ['250'].
-value('500') --> ['500'].
-% Add more values as needed.
+value(V) --> [V].
 
 unit(dollars) --> [dollars].
-% unit(cubic,feet) --> [cubic, feet].
+unit([cubic,feet]) --> [cubic, feet].
 
 
-
-
-student(susan_bones) --> [susan_bones].
-student(eli_olivander) --> [eli_olivander].
-student(vince_glover) --> [vince_glover].
-student(terry_boot) --> [terry_boot].
-
-student(duncan_inglebee) --> [duncan_inglebee].
-student(cho_chang) --> [cho_chang].
-student(draco_malfoy) --> [draco_malfoy].
-student(terence_higgs) --> [terence_higgs].
-
-student(daphne_greengrass) --> [daphne_greengrass].
-student(harry_potter) --> [harry_potter].
-student(ron_weasley) --> [ron_weasley].
-
-student(hermione_granger) --> [hermione_granger].
-
-item(a) --> [a].
-item(b) --> [b].
-item(c) --> [c].
-item(d) --> [d].
-
-item(v) --> [v].
-item(w) --> [w].
-
-item(x) --> [x].
-item(y) --> [y].
-item(z) --> [z].
-item(u) --> [u].
-
-cost('400') --> ['400'].
-cost('260') --> ['260'].
-cost('490') --> ['490'].
-cost('350') --> ['350'].
-
-cost('125') --> ['125'].
-cost('235') --> ['235'].
-cost('600') --> ['600'].
-cost('1200') --> ['1200'].
-
-cost('400') --> ['400'].
-cost('1000') --> ['1000'].
-cost('233') --> ['233'].
-
-cost('200') --> ['200'].
-cost('100') --> ['100'].
-cost('300') --> ['300'].
-
-
-volume('10') --> ['10'].
-volume('15') --> ['15'].
-volume('35') --> ['35'].
-volume('50') --> ['50'].
-
-volume('26') --> ['26'].
-volume('34') --> ['34'].
-volume('60') --> ['60'].
-
-
-volume('15') --> ['15'].
-volume('100') --> ['100'].
-volume('13') --> ['13'].
-
-
-volume('20') --> ['20'].
-volume('30') --> ['30'].
-volume('10') --> ['10'].
-
-house(gryffindor) --> [gryffindor].
-house(slytherin) --> [slytherin].
-house(ravenclaw) --> [ravenclaw].
-house(hufflepuff) --> [hufflepuff].
-
+student(S) --> [S].
+item(I) --> [I].
+cost(C) --> [C].
+volume(V) --> [V].
+house(H) --> [H].
 
 /* This receives the whole paragraph, and goes through and prints line by line */
 recurseStudent([]).
@@ -164,14 +79,7 @@ recurseStudent([X|Xs]) :-
         parse(House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2, X, []),
         write([House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2]), nl
     ),
-    % parse(Student, Item, Cost, Volume, X, []),
-    %write([Student, Item, Cost, Volume]), nl,
-
-    %parse(House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2, X, []),
-    %write([House, Attribute1, Comparison1, Than, Value1, Unit1, Attribute2, Comparison2, Than, Value2, Unit2]), nl,
     recurseStudent(Xs).
-
-
 
 main :- 
     current_prolog_flag(argv, [DataFile|_]),
